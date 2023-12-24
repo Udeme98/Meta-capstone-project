99fb08e881f80352ca10ff857049f233bb9def1a
Udeme98 <100492222+Udeme98@users.noreply.github.com>
2023-12-24 20:07:14 +0100

    used controlled component in the bookingform form

--- src/BookingPage.js
+++ src/BookingPage.js
@@ -1,16 +1,45 @@
+import { useState } from "react";
 import Navbar from "./Navbar";
 
 export default function BookingForm() {
+  const [formData, setFormData] = useState({
+    date: "",
+    time: "",
+    guest: "",
+    occasion: "",
+  });
+
+  function handleChange(event) {
+    setFormData((prevData) => {
+      return { ...prevData, [event.target.name]: event.target.value };
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(formData);
+  }
   return (
     <>
       <Navbar />
       <div className="form-page">
-        <form className="book-form">
+        <form className="book-form" onSubmit={handleSubmit}>
           <label htmlFor="res-date">Choose date:</label>
-          <input type="date" id="res-date" />
+          <input
+            type="date"
+            id="res-date"
+            name="date"
+            value={formData.date}
+            onChange={handleChange}
+          />
           <br />
           <label htmlFor="res-time">Choose time:</label>
-          <select id="res-time ">
+          <select
+            id="res-time"
+            name="time"
+            value={formData.time}
+            onChange={handleChange}
+          >
             <option>17:00</option>
             <option>18:00</option>
             <option>19:00</option>
@@ -20,10 +49,24 @@ export default function BookingForm() {
           </select>
           <br />
           <label htmlFor="guests">Number of guests:</label>
-          <input type="number" placeholder="1" min="1" max="10" id="guests" />
+          <input
+            type="number"
+            placeholder="1"
+            min="1"
+            max="10"
+            id="guests"
+            name="guest"
+            value={formData.guest}
+            onChange={handleChange}
+          />
           <br />
           <label htmlFor="occasion">Occasion: </label>
-          <select id="occasion">
+          <select
+            id="occasion"
+            name="occasion"
+            value={formData.occasion}
+            onChange={handleChange}
+          >
             <option>Birthday</option>
             <option>Anniversary</option>
           </select>
@@ -32,7 +75,6 @@ export default function BookingForm() {
           <button className="book-btn">Make Your reservation</button>
         </form>
       </div>
-      {/* <Footer /> */}
     </>
   );
 }
