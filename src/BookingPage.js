c7e7a0657f40b921c3e27eaec5a2356e591960f4
Udeme98 <100492222+Udeme98@users.noreply.github.com>
2023-12-27 06:12:31 +0100

    lifted the bookingform state up to use it in other components

--- src/BookingPage.js
+++ src/BookingPage.js
@@ -1,11 +1,25 @@
+import { useReducer } from "react";
 import BookingForm from "./BookingForm";
 import Navbar from "./Navbar";
 
 export default function BookingPage() {
+  const initializeTimes = () => ["17:00", "18:00", "19:00", "20:00", "21:00"];
+  const [availableTimes, dispatchTimes] = useReducer(
+    updateTimes,
+    [],
+    initializeTimes
+  );
+
+  function updateTimes(state, action) {
+    return initializeTimes();
+  }
   return (
     <>
       <Navbar />
-      <BookingForm />
+      <BookingForm
+        availableTimes={availableTimes}
+        dispatchTimes={dispatchTimes}
+      />
     </>
   );
 }
