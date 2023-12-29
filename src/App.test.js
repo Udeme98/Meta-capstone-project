f465f2892fe70e9f3ea288a5591db4c7364f9efa
Udeme98 <100492222+Udeme98@users.noreply.github.com>
2023-12-29 22:00:34 +0100

    Setting A booking API

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from "@testing-library/react";
-import BookingForm from "./BookingForm";
+// import { render, screen } from "@testing-library/react";
+// import BookingForm from "./BookingForm";
 
-test("renders learn react link", () => {
-  render(<BookingForm />);
-  const headingElement = screen.getByText("Make Your reservation");
-  expect(headingElement).toBeInTheDocument();
-});
+// import { initializeTimes } from "./BookingForm";
+
+// test("initializeTimes returns the correct expected value", () => {
+//   const expectedvalue = ["17:00", "18:00", "19:00", "20:00", "21:00"];
+
+//   const result = initializeTimes();
+
+//   expectedvalue(result).toEqual(expectedvalue);
+// });
--- src/BookingPage.js
+++ src/BookingPage.js
@@ -1,18 +1,47 @@
-import { useReducer } from "react";
+import { useEffect, useReducer } from "react";
 import BookingForm from "./BookingForm";
 import Navbar from "./Navbar";
 
 export default function BookingPage() {
-  const initializeTimes = () => ["17:00", "18:00", "19:00", "20:00", "21:00"];
+  // const initializeTimes = () => ["17:00", "18:00", "19:00", "20:00", "21:00"];
+  // const [availableTimes, dispatchTimes] = useReducer(
+  //   updateTimes,
+  //   [],
+  //   initializeTimes
+  // );
+
+  // function updateTimes(state, action) {
+  //   return initializeTimes();
+  // }
+
+  const initializeTimes = async () => {
+    const today = new Date();
+    // eslint-disable-next-line no-undef
+    const availableTimes = await fetchAPI(today);
+    return availableTimes;
+  };
+
   const [availableTimes, dispatchTimes] = useReducer(
     updateTimes,
     [],
     initializeTimes
   );
 
-  function updateTimes(state, action) {
-    return initializeTimes();
+  async function updateTimes(state, action) {
+    if (action.type === "UPDATE_TIMES") {
+      // eslint-disable-next-line no-undef
+      const availableTimes = await fetchAPI(action.date);
+      return availableTimes;
+    }
+    return state;
   }
+
+  useEffect(() => {
+    initializeTimes().then((times) =>
+      dispatchTimes({ type: "UPDATE_TIME", date: new Date() })
+    );
+  }, []);
+
   return (
     <>
       <Navbar />
