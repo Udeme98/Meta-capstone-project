874f706198132fae146c24d5aaa58532a7b87b3a
Udeme98 <Darealudeme1@gmail.com>
2024-04-07 13:15:13 +0100

    refactured the code to useReducer

--- src/Pages/BookingPage.js
+++ src/Pages/BookingPage.js
@@ -1,10 +1,14 @@
-import { useState } from "react";
+import { useState, useReducer } from "react";
 import BookingForm from "../Components/BookingForm";
 import Navbar from "../Components/Navbar";
 
+const timeArray = ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
+const initialState = timeArray;
+
+const reducer = () => {};
+
 export default function BookingPage() {
-  const [availableTimes, setAvailableTimes] = useState("");
-  const timeArray = ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
+  const [availableTimes, setAvailableTimes] = useReducer(reducer, initialState);
 
   return (
     <>
