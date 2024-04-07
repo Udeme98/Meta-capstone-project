ce60aba161adef3a9e1ad5757eb898a2708497eb
Udeme98 <Darealudeme1@gmail.com>
2024-04-07 07:49:51 +0100

    updated booking form

--- src/Components/BookingForm.js
+++ src/Components/BookingForm.js
@@ -1,22 +1,32 @@
 import { useState } from "react";
 
 export default function BookingForm() {
+  const [availableTimes, setAvailableTimes] = useState([
+    "17:00",
+    "18:00",
+    "19:00",
+    "20:00",
+    "21:00",
+    "22:00",
+  ]);
+
+  const dates = availableTimes.map((availableTime) => {
+    return <option>{availableTime}</option>;
+  });
+
+  // console.log(dates);
+
   function handleChange() {}
   return (
     <>
       <div className="form-page">
         <form className="book-form">
           <label htmlFor="res-date">Choose date:</label>
-          <input type="date" id="res-date" name="date" value="" />
+          <input type="date" id="res-date" name="date" />
           <br />
           <label htmlFor="res-time">Choose time:</label>
           <select id="res-time" name="time">
-            <option>17:00</option>
-            <option>18:00</option>
-            <option>19:00</option>
-            <option>20:00</option>
-            <option>21:00</option>
-            <option>22:00</option>
+            {dates}
           </select>
           <br />
           <label htmlFor="guests">Number of guests:</label>
